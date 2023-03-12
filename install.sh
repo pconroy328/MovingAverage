e3a761901414cb8238aaa9905386f31c91c8ad69
Patrick Conroy <patrick@conroy-family.net>
2023-03-12 12:15:52 -0600

    fixed

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo cp *.h /usr/local/include/.
 sudo cp dist/Debug/GNU-Linux/*.a /usr/local/lib/.
-sudo cp dist/Debug/GNU-Linux/*.so /usr/local/lib/.
+#sudo cp dist/Debug/GNU-Linux/*.so /usr/local/lib/.
+sudo ldconfig -v
